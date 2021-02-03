374d3f3395e907af032cf03cc093ebae645eaee0
Yogesh <you@example.com>
2021-02-03 11:49:51 +0530

    post create delete like unlike added

--- client/src/store.js
+++ client/src/store.js
@@ -4,11 +4,13 @@ import thunk from 'redux-thunk'
 import alertReducer from './reducers/alertReducer'
 import authReducer from './reducers/authReducers'
 import profileReducer from './reducers/profileReducer'
+import postReducer from './reducers/postReducer'
 
 const reducer = combineReducers({
 	alertReducer,
 	authReducer,
 	profileReducer,
+	postReducer,
 })
 
 const initialState = {}
